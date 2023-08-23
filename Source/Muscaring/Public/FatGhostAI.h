370a33860510e623788163d4134c9df0b7c99a14
ZeusSatake <22ci0503@jec.ac.jp>
2023-08-23 12:08:01 +0900

    Collisionをカスタムにしてすり抜けと視覚を正常に動作するように

---
+++ Source/Muscaring/Public/FatGhostAI.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GhostAI.h"
+#include "FatGhostAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUSCARING_API AFatGhostAI : public AGhostAI
+{
+	GENERATED_BODY()
+public:
+	AFatGhostAI();
+private:
+
+	//AIÇÃpawnÇï€éù
+	virtual void OnPossess(APawn* InPawn) override;
+
+	//AIÇÃpawnÇâèú
+	virtual void OnUnPossess() override;
+
+	//BeginPlay
+	virtual void BeginPlay() override;
+
+	//Tick
+	virtual void Tick(float DeltaSeconds) override;
+};
