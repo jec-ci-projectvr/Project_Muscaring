508125de52fe2f9f1d2113c80fe05914f7264705
ZeusSatake <22ci0503@jec.ac.jp>
2023-09-13 17:18:46 +0900

    TutorialGhostの改修

--- Source/Muscaring/Private/TutorialGhost.cpp
+++ Source/Muscaring/Private/TutorialGhost.cpp
@@ -39,15 +39,64 @@ void ATutorialGhost::ChangeMoveSpeed()
     else if (GetState() == GhostState::Swoon)
 		GetCharacterMovement()->MaxWalkSpeed = 0.f;
 }
+void ATutorialGhost::ChangeExpression()
+{
+	switch (GetState())
+	{
+	case GhostState::Approach:
+		GetMesh()->SetMaterial(0, LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN1")));
+		//GetMesh()->SetMaterial(0, GetMaterials()[0]);
+		break;
+	case GhostState::Scare:
+		GetMesh()->SetMaterial(0, LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN2")));
+		//GetMesh()->SetMaterial(0, GetMaterials()[1]);
+		break;
+	case GhostState::Escape:
+		GetMesh()->SetMaterial(0, LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN3")));
+		//GetMesh()->SetMaterial(0, GetMaterials()[1]);
+		break;
+	case GhostState::Swoon:
+		GetMesh()->SetMaterial(0, LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN3")));
+		//GetMesh()->SetMaterial(0, GetMaterials()[2]);
+		break;
+	default:
+		//GetMesh()->SetMaterial(0, GetMaterials()[0]);
+		break;
+	}
+}
+void ATutorialGhost::LoadAllExpression()
+{
+	GetMaterials().Add(LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN1")));
+	GetMaterials().Add(LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN2")));
+	GetMaterials().Add(LoadObject<UMaterial>(nullptr, TEXT("/Game/Characters/Ghosts/M_GN3")));
+}
 void ATutorialGhost::ListenSnapFingers()
 {
-	snapFingersFlag = true;
-	ChangeState();
-	ChangeMoveSpeed();
+	//プレイヤーとの距離を計測(高さを含まない)
+	const float distance = FVector::Dist2D(GetPlayer()->GetActorLocation(), GetActorLocation());
+	//聞こえる範囲内であれば実行
+	if (GetState() != GhostState::Idle)
+	{
+		UKismetSystemLibrary::PrintString(GetWorld(), FString::Printf(TEXT("scarePoint:%d"), GetScarePoint()));
+		ChangeState();
+		ChangeMoveSpeed();
+		IInterfaceGhostState::Execute_SetFakeOut(GetGhostAI(), true);
+		IInterfaceGhostState::Execute_SetGhostState(GetGhostAI(), GetState());
+		snapFingersFlag = true;
+	}
 }
 void ATutorialGhost::ListenFakeOut()
 {
-	fakeOutFlag = true;
-	ChangeState();
-	ChangeMoveSpeed();
+	//プレイヤーとの距離を計測(高さを含まない)
+	const float distance = FVector::Dist2D(GetPlayer()->GetActorLocation(), GetActorLocation());
+	//聞こえる範囲内であれば実行
+	if (GetState() != GhostState::Idle)
+	{
+		UKismetSystemLibrary::PrintString(GetWorld(), FString::Printf(TEXT("scarePoint:%d"), GetScarePoint()));
+		ChangeState();
+		ChangeMoveSpeed();
+		IInterfaceGhostState::Execute_SetFakeOut(GetGhostAI(), true);
+		IInterfaceGhostState::Execute_SetGhostState(GetGhostAI(), GetState());
+		fakeOutFlag = true;
+	}
 }
